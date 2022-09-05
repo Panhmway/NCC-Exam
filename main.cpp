8e111a6f3a071a7734e71490790c31b83cc115af
Htet Lin Aung <jokejoke186@gmail.com>
2022-09-05 07:53:08 +0630

    cStringFun

--- main.cpp
+++ main.cpp
@@ -55,6 +55,20 @@ int main(){
 
     printf(" Sizeof arr %d\n",sizeof(totalArary));
 
+    char myarr[10]={'N','C','C'};
+
+    char myarr2[10]= {'G','E','n','i','u','s'};
+
+    char myString[100];
+    strcpy(myString,myarr);
+    strcpy(myString,myarr2);
+
+    printf("Data: %s\n",myString);
+
+    strcat(myarr,myarr2);
+
+    printf("Data: %s",myarr);
+    printf(" length Data: %d", strlen(myarr));
 
     return 0;
 }
